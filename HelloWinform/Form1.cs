2315a59bc03183ac8825c24bc4643ed1fa868958
정민 오 <9970mia@naver.com>
2021-03-31 19:43:40 +0900

    버튼 추가 및 버튼 클릭 핸들러 코드 작성

--- HelloWinform/Form1.cs
+++ HelloWinform/Form1.cs
@@ -17,5 +17,16 @@ namespace HelloWinform
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(sender.Equals(button1))
+            {
+                MessageBox.Show("첫 번째 버튼 와우!");
+            }
+            else
+            {
+                MessageBox.Show("두 번째 버튼 와우!");
+            }
+        }
     }
 }
